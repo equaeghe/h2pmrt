3e58a7d96dea1568f5b277de666392da317439ad
Erik Quaeghebeur <equaeghe@users.noreply.github.com>
2025-10-12 17:08:53 +0200

    add existing files

---
+++ h2pmrt/__init__.py
@@ -0,0 +1,22 @@
+import bs4
+
+import h2pmrt.text as text
+import h2pmrt.css as css
+import h2pmrt.undo as undo
+import h2pmrt.html as html
+
+def convert(html_string: str) -> str:
+    """Convert html to poor man's rich text"""
+    soup = bs4.BeautifulSoup(text.cleanup(html_string), "html5lib")
+    soup = css.css2tags(soup)
+    if soup.body:
+        soup = soup.body
+    undo.ms_sender_identification(soup)
+    undo.link_rewriting(soup)
+    html.remove_empty(soup)
+    html.unwrap_spans(soup)
+    html.sweat_whitespace(soup)
+    html.remove_empty(soup)
+    html.merge_markup(soup)
+    html.tags2text(soup)
+    return str(soup.string)
---
+++ h2pmrt/css.py
@@ -0,0 +1,48 @@
+import bs4
+import tinycss2 as tc2
+
+def css2tags(soup: bs4.BeautifulSoup) -> bs4.BeautifulSoup:
+    """Convert CSS styles to html markup"""
+
+    # Inline style blocks
+    # TODO: use Pyhon bindings of https://github.com/Stranger6667/css-inline
+
+    for tag in soup.css.select("[style]"):
+        # Find rules applied
+        applied = {}
+        declarations = tc2.parse_blocks_contents(tag["style"],
+            skip_comments=True, skip_whitespace=True)
+        for decl in declarations:
+            if decl.type == "declaration":
+                applied[decl.name] = [value.lower_value for value in decl.value
+                                      if isinstance(value, tc2.ast.IdentToken)]
+        del tag["style"]
+        # Apply formatting
+        if any(value.startswith("bold")
+               for value in applied.get("font-weight", [])):
+            wrap_children(soup, tag, "b")
+            # tag = tag.wrap(soup.new_tag("b"))
+        if any(value in {"italic", "oblique"}
+               for value in applied.get("font-style", [])):
+            wrap_children(soup, tag, "i")
+            # tag = tag.wrap(soup.new_tag("i"))
+        if any(value.startswith("underline")
+               for value in applied.get("text-decoration", [])):
+            wrap_children(soup, tag, "u")
+            # tag = tag.wrap(soup.new_tag("u"))
+        for border_key in {"border", "border-top"}:
+            if any(value in {"solid", "dashed", "double"}
+                   for value in applied.get(border_key, [])):
+                tag.insert_before(soup.new_tag("hr"))
+        for border_key in {"border", "border-bottom"}:
+            if any(value in {"solid", "dashed", "double"}
+                   for value in applied.get(border_key, [])):
+                tag.insert_after(soup.new_tag("hr"))
+    return soup
+
+def wrap_children(soup: bs4.BeautifulSoup,
+                  parent_tag: bs4.Tag, wrapping_tag_name: str):
+    wrapper = soup.new_tag(wrapping_tag_name)
+    for content in reversed(parent_tag.contents):
+        wrapper.insert(0, content.extract())
+    parent_tag.append(wrapper)
