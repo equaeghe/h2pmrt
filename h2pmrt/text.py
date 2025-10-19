65d9764d41f45e2600a0fa3c9e2eae16e34a241a
Erik Quaeghebeur <equaeghe@users.noreply.github.com>
2025-10-19 15:40:08 +0200

    Add cleanup docstring

--- h2pmrt/text.py
+++ h2pmrt/text.py
@@ -6,6 +6,7 @@ re_brn = re.compile(r"(<br ?/?>)\n+")
 
 
 def cleanup(html_string: str) -> str:
+    """Clean up whitespace"""
     html_string = re_brn.sub(r"\1", html_string)
     html_string = mh.minify(html_string)
     return html_string
