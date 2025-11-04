a5532ac2cb8422f1865b30f173f907891c30c25d
Erik Quaeghebeur <equaeghe@users.noreply.github.com>
2025-11-04 22:02:19 +0100

    Make MS SenderIdentification string removal more robust

--- h2pmrt/undo.py
+++ h2pmrt/undo.py
@@ -2,9 +2,36 @@ import bs4
 
 
 def ms_sender_identification(soup: bs4.BeautifulSoup):
-    a = soup.find("a", href="https://aka.ms/LearnAboutSenderIdentification")
-    if a is not None and a.parent is not None:
-        a.parent.decompose()
+    """Remove MS SenderIdentification string(s)"""
+    for a in soup.select("a[href='https://aka.ms/LearnAboutSenderIdentification']"):
+        # find first br before (or first sibling) and after (or last sibling)
+        before = a
+        while not (before and before.name == "br"):
+            if before.previous_sibling is None:
+                break
+            else:
+                before = before.previous_sibling
+        after = a
+        while not (after and after.name == "br"):
+            if after.next_sibling is None:
+                break
+            else:
+                after = after.next_sibling
+        # decompose all tags between before and after
+        match (before.name, after.name):
+            case ("br", "br"):
+                tag = before.next_sibling
+            case (_, "br") | ("br", _):
+                tag = before
+            case (_, _):
+                a.parent.decompose()
+                return
+        completed = False
+        while not completed:
+            if tag is after:
+                completed = True
+            tag = tag.next_sibling
+            tag.previous_sibling.decompose()
 
 
 def link_rewriting(soup: bs4.BeautifulSoup):
